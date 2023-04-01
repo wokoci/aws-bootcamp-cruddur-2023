24eec72f2a077cbaeb12b27a407d307318e10644
Jeffrey Wokocha <wokoci@gmail.com>
2023-04-01 13:29:24 +0000

    testing data passage to home activity page

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -7,26 +7,30 @@ from lib.db import pool
 class HomeActivities:
   def run(cognito_user_id = None):
       print("---INSIDE HOME ACTIVITY-------")
+      print("---INSIDE HOME ACTIVITY----22--")
+      now = datetime.now(timezone.utc).astimezone()
+      print( "This is the retrun that is not reached???????????")
+      print(cognito_user_id, "<<<<<<<<---------")
     # with tracer.start_as_current_span("home-activity") as outer_span:
     #   with tracer.start_as_current_span("inner-home-activity") as inner_span:
     #       outer_span.set_attribute("entering call", True)
     #       inner_span.set_attribute("in call", True)
-    #       now = datetime.now(timezone.utc).astimezone()
-
-      sql = """ 
-      SELECT * FROM activities;  
-      """
           
-      with pool.connection() as conn:
-        with conn.cursor() as cur:
-          cur.execute(sql)
-          # this will return a tuple
-          # the first field being the data
-          rows = cur.fetchall()
-          for row in rows:
-            print("------11--22-----")
-            json = cur.fetchall()
-            return json[0]
+
+      # sql = """ 
+      # SELECT * FROM activities;  
+      # """
+          
+      # with pool.connection() as conn:
+      #   with conn.cursor() as cur:
+      #     cur.execute(sql)
+      #     # this will return a tuple
+      #     # the first field being the data
+      #     rows = cur.fetchall()
+      #     for row in rows:
+      #       print("------11--22-----")
+      #       json = cur.fetchall()
+      #       return json[0]
 
       # handle = results[0]['handle']
       # span = trace.get_current_span()
