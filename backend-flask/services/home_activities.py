8362bf68134a41a05685f5f5bd78bbdea2c4983a
Jeffrey Wokocha <wokoci@gmail.com>
2023-04-04 21:34:34 +0000

    add hone and object sql files

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,12 +1,14 @@
 from datetime import datetime, timedelta, timezone
 from lib.db import db
+import logging
+logger = logging.getLogger("LOGGER")
 
 # from opentelemetry import trace
 # tracer = trace.get_tracer("home.activity.tracer")
 class HomeActivities:
   def run(cognito_user_id = None):
       print("******HOME ACTIVITY? **********")
-    #logger.info("HomeActivities")
+      logger.info("HomeActivities")
       # with tracer.start_as_current_span("home-activites-mock-data"):
 
       results = db.query_array_json("""
