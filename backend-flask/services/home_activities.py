845ed2d051b49471f708656351a3c88984aab6f9
Jeffrey Wokocha <wokoci@gmail.com>
2023-04-04 22:33:39 +0000

    update create activity

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -11,20 +11,6 @@ class HomeActivities:
       logger.info("HomeActivities")
       # with tracer.start_as_current_span("home-activites-mock-data"):
 
-      results = db.query_array_json("""
-      SELECT
-        activities.uuid,
-        users.display_name,
-        users.handle,
-        activities.message,
-        activities.replies_count,
-        activities.reposts_count,
-        activities.likes_count,
-        activities.reply_to_activity_uuid,
-        activities.expires_at,
-        activities.created_at
-      FROM public.activities
-      LEFT JOIN public.users ON users.uuid = activities.user_uuid
-      ORDER BY activities.created_at DESC
-      """)
+      sql = db.template('activities', 'home')
+      results = db.query_array_json(sql)
       return results
