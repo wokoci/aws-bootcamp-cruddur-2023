082030d44c50b8060fc990d198ed27766268d756
Jeffrey Wokocha <wokoci@gmail.com>
2023-04-05 20:10:46 +0000

    add activities functionality

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,14 +1,15 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-   ('Jeffrey Wokocha', 'jeffreyw' ,'MOCK'),
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'MOCK');
+  ('Andrew Brown', 'andrewbrown', 'andrewbrown@exam.co' ,'MOCK'),
+  ('Andrew Bayko', 'bayko','bayko@exam.co' ,'MOCK'),
+   ('Jeffrey Wokocha', 'jeffrey','wokoci@gmail.com' ,'MOCK'),;
+
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'jeffreyw' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
